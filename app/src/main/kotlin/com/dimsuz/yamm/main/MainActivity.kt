3635dfcb9583f29058d5d4dab5f2e4e579ae3e94
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-07 20:14:02 +0200

    Configure project to use custom font
    Bundle Roboto along with the app to avoid weird fonts on devices which
    do not have Robot preinstalled (Samsung?)

--- app/src/main/kotlin/com/dimsuz/yamm/main/MainActivity.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/main/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.dimsuz.yamm.main
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.dimsuz.yamm.R
 
 class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_content_main)
+  }
 }
