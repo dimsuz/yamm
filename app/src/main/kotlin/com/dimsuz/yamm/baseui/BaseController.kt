00b635eb2e5111613187a4fdc374eaa8c585bd8b
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-12 22:48:52 +0200

    Use getViewLayout instead of createView
    Proved to be one-liner in other projects

--- app/src/main/kotlin/com/dimsuz/yamm/baseui/BaseController.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/baseui/BaseController.kt
@@ -1,26 +1,29 @@
 package com.dimsuz.yamm.baseui
 
 import android.os.Bundle
+import android.support.annotation.LayoutRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
 
 abstract class BaseController : Controller, ResettableSupport {
+  final override val refManager = ResettableReferencesManager()
   final override var bindPropsRootView: View? = null
 
   constructor()
   constructor(args: Bundle) : super(args)
 
   final override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
-    val rootView = createView(inflater, container)
+    val rootView = inflater.inflate(getViewLayout(), container, false)
     bindPropsRootView = rootView
     // initialization can happen only after bindPropsRootView is assigned, this is required for BindView delegate to work
     initializeView(rootView)
     return rootView
   }
 
-  abstract fun createView(inflater: LayoutInflater, container: ViewGroup): View
+  @LayoutRes
+  abstract fun getViewLayout(): Int
   abstract fun initializeView(rootView: View)
 
   override fun onDestroyView(view: View) {
