4a53a63b2513c6060298898d804948c793f46f33
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-18 00:23:35 +0200

    Add various useful extensions for Controller and Context

--- app/src/main/kotlin/com/dimsuz/yamm/baseui/util/ControllerExtensions.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/baseui/util/ControllerExtensions.kt
@@ -1,6 +1,53 @@
 package com.dimsuz.yamm.baseui.util
 
 import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
+import com.bluelinelabs.conductor.changehandler.FadeChangeHandler
+import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
+import com.bluelinelabs.conductor.changehandler.VerticalChangeHandler
+import com.dimsuz.yamm.util.appScope
 
 inline val Controller.resourcesUnsafe get() = this.resources!!
+inline val Controller.activityUnsafe get() = this.activity!!
+inline val Controller.appScope get() = activityUnsafe.appScope
 
+private const val SLIDE_BOTTOM_PUSH_ANIM_DURATION = 300L
+private const val SLIDE_BOTTOM_POP_ANIM_DURATION = 200L
+private const val SLIDE_HORIZONTAL_PUSH_ANIM_DURATION = 300L
+private const val SLIDE_HORIZONTAL_POP_ANIM_DURATION = 200L
+private const val SLIDE_FADE_PUSH_ANIM_DURATION = 300L
+private const val SLIDE_FADE_POP_ANIM_DURATION = 200L
+
+fun Controller.obtainVerticalTransaction(): RouterTransaction {
+  return RouterTransaction
+    .with(this)
+    .pushChangeHandler(VerticalChangeHandler(SLIDE_BOTTOM_PUSH_ANIM_DURATION))
+    .popChangeHandler(VerticalChangeHandler(SLIDE_BOTTOM_POP_ANIM_DURATION))
+}
+
+fun Controller.obtainHorizontalTransaction(): RouterTransaction {
+  return RouterTransaction
+    .with(this)
+    .pushChangeHandler(HorizontalChangeHandler(SLIDE_HORIZONTAL_PUSH_ANIM_DURATION))
+    .popChangeHandler(HorizontalChangeHandler(SLIDE_HORIZONTAL_POP_ANIM_DURATION))
+}
+
+fun Controller.obtainFadeTransaction(): RouterTransaction {
+  return RouterTransaction
+    .with(this)
+    .pushChangeHandler(FadeChangeHandler(SLIDE_FADE_PUSH_ANIM_DURATION))
+    .popChangeHandler(FadeChangeHandler(SLIDE_FADE_POP_ANIM_DURATION))
+}
+
+fun Router.pushControllerHorizontal(controller: Controller) {
+  return pushController(controller.obtainHorizontalTransaction())
+}
+
+fun Router.pushControllerVertical(controller: Controller) {
+  return pushController(controller.obtainVerticalTransaction())
+}
+
+fun Router.pushControllerFade(controller: Controller) {
+  return pushController(controller.obtainFadeTransaction())
+}
--- app/src/main/kotlin/com/dimsuz/yamm/util/ContextExtensions.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/util/ContextExtensions.kt
@@ -3,4 +3,4 @@ package com.dimsuz.yamm.util
 import android.content.Context
 import toothpick.Toothpick
 
-internal val Context.appScope get() = Toothpick.openScope(this.applicationContext)
+inline val Context.appScope get() = Toothpick.openScope(this.applicationContext)
