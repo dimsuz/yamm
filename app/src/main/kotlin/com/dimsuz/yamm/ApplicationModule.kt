5fd4cd7d0a84c3589e3f540e08958a5a7e409aa3
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-18 00:22:58 +0200

    Bind YammApplication to application instance

--- app/src/main/kotlin/com/dimsuz/yamm/ApplicationModule.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/ApplicationModule.kt
@@ -8,6 +8,7 @@ import toothpick.config.Module
 internal class ApplicationModule(application: YammApplication) : Module() {
   init {
     bind(Context::class.java).toInstance(application)
+    bind(YammApplication::class.java).toInstance(application)
     // not making it singleton, will be recreated each time it's needed.
     // Currently I think it will be rarely needed, but if this will become wrong, rethink
     bind(AppConfig::class.java).to(PrefsBasedAppConfig::class.java)
