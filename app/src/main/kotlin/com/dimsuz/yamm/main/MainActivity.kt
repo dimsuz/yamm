510b8ff67c70ebad756db9edd6e2ea9813636ffc
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-20 00:57:22 +0200

    Proceed to LoginScreen until logged in

--- app/src/main/kotlin/com/dimsuz/yamm/main/MainActivity.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/main/MainActivity.kt
@@ -5,15 +5,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.dimsuz.yamm.R
 import com.dimsuz.yamm.login.LoginActivity
-import com.dimsuz.yamm.util.AppConfig
-import com.dimsuz.yamm.util.appScope
-import com.dimsuz.yamm.util.instance
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    val appConfig = appScope.instance<AppConfig>()
-    if (appConfig.getServerUrl() == null) {
+    val isLoggedIn = false // TODO
+    if (!isLoggedIn) {
       val intent = Intent(this, LoginActivity::class.java)
       startActivity(intent)
       finish()
