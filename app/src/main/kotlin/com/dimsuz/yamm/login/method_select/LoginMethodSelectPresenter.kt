b806f167ba111433ef5ccfc0802dff508e94afdc
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-21 17:52:03 +0200

    Extract a routing state reducer into a separate function

--- app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelectPresenter.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelectPresenter.kt
@@ -1,7 +1,7 @@
 package com.dimsuz.yamm.login.method_select
 
 import com.dimsuz.yamm.baseui.BaseMviPresenter
-import com.dimsuz.yamm.baseui.models.ViewIntentResult
+import com.dimsuz.yamm.baseui.RoutingAction
 import com.dimsuz.yamm.network.MattermostService
 import com.dimsuz.yamm.util.AppSchedulers
 import io.reactivex.Observable
@@ -13,13 +13,16 @@ private object ServerConfigLoading : ScreenEvent()
 class LoginMethodSelectPresenter @Inject constructor(private val mattermostService: MattermostService,
                                                      schedulers: AppSchedulers)
   : BaseMviPresenter<LoginMethodSelect.View, LoginMethodSelect.ViewState, ScreenEvent>(schedulers) {
-
   override fun createIntents(): List<Observable<out ScreenEvent>> {
     return emptyList()
   }
 
-  override fun viewStateReducer(previousState: LoginMethodSelect.ViewState, event: ScreenEvent): ViewIntentResult<LoginMethodSelect.ViewState> {
-    return ViewIntentResult(previousState, null)
+  override fun viewStateReducer(previousState: LoginMethodSelect.ViewState, event: ScreenEvent): LoginMethodSelect.ViewState {
+    return previousState
+  }
+
+  override fun routingStateReducer(previousState: LoginMethodSelect.ViewState, newState: LoginMethodSelect.ViewState, event: ScreenEvent): RoutingAction? {
+    return null
   }
 
   override fun createInitialState(): LoginMethodSelect.ViewState {
