6139f92115701e911e0ed7cfc25cc407744f692d
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-21 18:22:15 +0200

    Add basic server config UI flow to LoginMethodSelect

--- app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelect.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelect.kt
@@ -4,5 +4,13 @@ import com.dimsuz.yamm.baseui.MviView
 
 interface LoginMethodSelect {
   interface View : MviView<ViewState>
-  class ViewState
+
+  data class ViewState(
+    val showProgressBar: Boolean,
+    val loadingError: String?,
+    val showGitlabSignIn: Boolean,
+    val showEmailSignIn: Boolean
+  ) {
+    fun clearTransientState() = copy(showProgressBar = false, loadingError = null)
+  }
 }
--- app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelectPresenter.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelectPresenter.kt
@@ -2,23 +2,47 @@ package com.dimsuz.yamm.login.method_select
 
 import com.dimsuz.yamm.baseui.BaseMviPresenter
 import com.dimsuz.yamm.baseui.RoutingAction
+import com.dimsuz.yamm.domain.ServerConfig
 import com.dimsuz.yamm.network.MattermostService
 import com.dimsuz.yamm.util.AppSchedulers
+import com.dimsuz.yamm.util.ErrorDetailsExtractor
 import io.reactivex.Observable
+import timber.log.Timber
 import javax.inject.Inject
 
 sealed class ScreenEvent
 private object ServerConfigLoading : ScreenEvent()
+private data class ServerConfigLoaded(val serverConfig: ServerConfig) : ScreenEvent()
+private data class ServerConfigLoadFailed(val error: Throwable) : ScreenEvent()
 
 class LoginMethodSelectPresenter @Inject constructor(private val mattermostService: MattermostService,
+                                                     private val errorDetailsExtractor: ErrorDetailsExtractor,
                                                      schedulers: AppSchedulers)
   : BaseMviPresenter<LoginMethodSelect.View, LoginMethodSelect.ViewState, ScreenEvent>(schedulers) {
+
   override fun createIntents(): List<Observable<out ScreenEvent>> {
-    return emptyList()
+    val fetchServerConfigFlow = intent { fetchServerConfigFlow() }
+    return listOf(fetchServerConfigFlow)
+  }
+
+  private fun fetchServerConfigFlow(): Observable<ScreenEvent> {
+    return mattermostService.getServerConfig()
+      .map<ScreenEvent> { ServerConfigLoaded(it) }
+      .onErrorReturn { ServerConfigLoadFailed(it) }
+      .toObservable()
+      .startWith(ServerConfigLoading)
   }
 
   override fun viewStateReducer(previousState: LoginMethodSelect.ViewState, event: ScreenEvent): LoginMethodSelect.ViewState {
-    return previousState
+    Timber.d("computing state for event $event")
+    val nextStateDraft = previousState.clearTransientState()
+    return when (event) {
+      is ServerConfigLoading -> nextStateDraft.copy(showProgressBar = true)
+      is ServerConfigLoaded -> nextStateDraft.copy(
+        showGitlabSignIn = event.serverConfig.isGitlabSignInEnabled,
+        showEmailSignIn = event.serverConfig.isEmailSignInEnabled)
+      is ServerConfigLoadFailed -> nextStateDraft.copy(loadingError = errorDetailsExtractor.extractErrorText(event.error))
+    }
   }
 
   override fun routingStateReducer(previousState: LoginMethodSelect.ViewState, newState: LoginMethodSelect.ViewState, event: ScreenEvent): RoutingAction? {
@@ -26,7 +50,12 @@ class LoginMethodSelectPresenter @Inject constructor(private val mattermostServi
   }
 
   override fun createInitialState(): LoginMethodSelect.ViewState {
-    return LoginMethodSelect.ViewState()
+    return LoginMethodSelect.ViewState(
+      showProgressBar = false,
+      loadingError = null,
+      showEmailSignIn = false,
+      showGitlabSignIn = false
+    )
   }
 
 }
