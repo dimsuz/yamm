a7f9d78a7e65b6c93c4952b0ef54ed8374eb52ca
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-21 18:21:07 +0200

    Fix MattermostService.getServerConfig() to return Single

--- app-network/src/main/kotlin/com/dimsuz/yamm/network/MattermostService.kt
+++ app-network/src/main/kotlin/com/dimsuz/yamm/network/MattermostService.kt
@@ -2,9 +2,10 @@ package com.dimsuz.yamm.network
 
 import com.dimsuz.yamm.domain.ServerConfig
 import com.dimsuz.yamm.network.mappers.toDomainModel
+import io.reactivex.Single
 
 class MattermostService internal constructor(private val serviceApi: MattermostServiceApi) {
-  fun getServerConfig(): ServerConfig {
-    return serviceApi.getServerConfig().toDomainModel()
+  fun getServerConfig(): Single<ServerConfig> {
+    return serviceApi.getServerConfig().map { it.toDomainModel() }
   }
 }
--- app-network/src/main/kotlin/com/dimsuz/yamm/network/MattermostServiceApi.kt
+++ app-network/src/main/kotlin/com/dimsuz/yamm/network/MattermostServiceApi.kt
@@ -1,11 +1,12 @@
 package com.dimsuz.yamm.network
 
 import com.dimsuz.yamm.network.models.ServerConfigJson
+import io.reactivex.Single
 import retrofit2.http.GET
 
 internal interface MattermostServiceApi {
 
   @GET("config/client?format=old")
-  fun getServerConfig(): ServerConfigJson
+  fun getServerConfig(): Single<ServerConfigJson>
 
 }
