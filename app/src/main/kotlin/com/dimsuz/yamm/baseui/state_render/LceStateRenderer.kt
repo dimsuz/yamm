4b93631524d2409b9933a47a7b23f46dcdfc6005
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-22 16:33:39 +0200

    Add basic state switching to LceStateRenderer

--- app/src/main/kotlin/com/dimsuz/yamm/baseui/state_render/LceStateRenderer.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/baseui/state_render/LceStateRenderer.kt
@@ -3,6 +3,7 @@ package com.dimsuz.yamm.baseui.state_render
 import android.view.View
 import android.view.ViewGroup
 import com.dimsuz.yamm.baseui.HasLceState
+import com.dimsuz.yamm.baseui.util.isVisible
 
 class LceStateRenderer<in S : HasLceState<*, *>>(
   private val contentViewId: Int,
@@ -11,11 +12,13 @@ class LceStateRenderer<in S : HasLceState<*, *>>(
 
   private var progressBar: View? = null
   private var contentView: ViewGroup? = null
+  private var errorView: View? = null
   private var additionalContentViews: List<View>? = null
 
   override fun onViewCreated(view: View) {
     progressBar = view.findViewById(progressBarViewId)
     contentView = view.findViewById(contentViewId)
+    errorView = contentView
     additionalContentViews = additionalContentViewIds?.map { view.findViewById<View>(it) }
   }
 
@@ -24,12 +27,27 @@ class LceStateRenderer<in S : HasLceState<*, *>>(
       && previousState.contentLoadingError == state.contentLoadingError) {
       return
     }
-    // TODO
+
+    progressBar?.isVisible = state.contentLoadingError == null && state.showProgressBar
+    val contentVisible = state.contentLoadingError == null && !state.showProgressBar
+    contentView?.isVisible = contentVisible
+    additionalContentViews?.map { it.isVisible = contentVisible }
+
+    if (state.contentLoadingError != null) {
+      ensureErrorViewCreated()
+    }
+    errorView?.isVisible = state.contentLoadingError != null
   }
 
   override fun onViewDestroyed() {
     progressBar = null
     contentView = null
+    errorView = null
     additionalContentViews = null
   }
+
+  private fun ensureErrorViewCreated() {
+    // TODO
+  }
+
 }
