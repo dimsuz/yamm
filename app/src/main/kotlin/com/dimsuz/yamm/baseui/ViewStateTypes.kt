614ad4a141ecfb949b81af50fcddce65e3a80af2
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-22 12:37:28 +0200

    Add HasLceState and HasTransientState state types

---
+++ app/src/main/kotlin/com/dimsuz/yamm/baseui/ViewStateTypes.kt
@@ -0,0 +1,10 @@
+package com.dimsuz.yamm.baseui
+
+interface HasLceState<out E, S : HasLceState<E, S>> : HasTransientState<S> {
+  val showProgressBar: Boolean
+  val contentLoadingError: E?
+}
+
+interface HasTransientState<out S : HasTransientState<S>> {
+  fun clearTransientState(): S
+}
--- app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelect.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/login/method_select/LoginMethodSelect.kt
@@ -1,16 +1,17 @@
 package com.dimsuz.yamm.login.method_select
 
+import com.dimsuz.yamm.baseui.HasLceState
 import com.dimsuz.yamm.baseui.MviView
 
 interface LoginMethodSelect {
   interface View : MviView<ViewState>
 
   data class ViewState(
-    val showProgressBar: Boolean,
-    val loadingError: String?,
+    override val showProgressBar: Boolean,
+    override val contentLoadingError: String?,
     val showGitlabSignIn: Boolean,
     val showEmailSignIn: Boolean
-  ) {
-    fun clearTransientState() = copy(showProgressBar = false, loadingError = null)
+  ) : HasLceState<String, ViewState> {
+    override fun clearTransientState() = copy(showProgressBar = false, contentLoadingError = null)
   }
 }
