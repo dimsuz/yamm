d31d2eb354b6f153b905539bb8f31bfc9b94f67d
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-31 21:39:22 +0200

    Out of cicerone navigators leave only ControllerNavigator

--- app/src/main/kotlin/com/dimsuz/yamm/navigation/ActivityFactory.kt
+++
@@ -1,8 +0,0 @@
-package com.dimsuz.yamm.navigation
-
-import android.content.Context
-import android.content.Intent
-
-interface ActivityFactory {
-  fun createIntent(context: Context, screenKey: String, payload: Any?): Intent
-}
--- app/src/main/kotlin/com/dimsuz/yamm/navigation/ControllerNavigator.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/navigation/ControllerNavigator.kt
@@ -1,18 +1,18 @@
 package com.dimsuz.yamm.navigation
 
 import com.bluelinelabs.conductor.Router
+import ru.terrakok.cicerone.Navigator
 import ru.terrakok.cicerone.commands.*
 
-class ControllerNavigatorHelper(private val conductorRouter: Router,
-                                private val controllerFactory: ControllerFactory) {
+class ControllerNavigator(private val conductorRouter: Router,
+                          private val controllerFactory: ControllerFactory): Navigator {
 
-  fun applyCommand(command: Command): Boolean {
-    var handled = true
+  override fun applyCommand(command: Command) {
     when (command) {
       is Back -> conductorRouter.popCurrentController()
       is BackTo -> {
         conductorRouter.popToTag(command.screenKey)
-          .also { popped -> check(popped, { "routing back to ${command.screenKey} failed, no such tag" })}
+          .also { popped -> check(popped, { "routing back to ${command.screenKey} failed, no such tag" }) }
       }
       is Forward -> {
         val controller = controllerFactory.createController(command.screenKey, command.transitionData)
@@ -24,8 +24,6 @@ class ControllerNavigatorHelper(private val conductorRouter: Router,
         val transaction = controllerFactory.createReplaceTransaction(command.screenKey, command.transitionData, controller)
         conductorRouter.replaceTopController(transaction)
       }
-      else -> { handled = false }
     }
-    return handled
   }
 }
