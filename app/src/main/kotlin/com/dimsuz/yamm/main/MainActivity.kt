eb4c0c4a1398ef6b6e13a6d52cec945aa8b001a0
Dmitry Suzdalev <dimsuz@gmail.com>
2017-10-31 21:35:39 +0200

    Remove LoginActivity, put login controllers into MainActivity

--- app/src/main/kotlin/com/dimsuz/yamm/main/MainActivity.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/main/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.dimsuz.yamm.main
 
-import android.content.Intent
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import com.dimsuz.yamm.R
-import com.dimsuz.yamm.login.LoginActivity
+import com.bluelinelabs.conductor.Controller
+import com.dimsuz.yamm.baseui.BaseControllerActivity
+import com.dimsuz.yamm.data.sources.network.session.SessionManager
+import com.dimsuz.yamm.login.method_select.LoginMethodSelectController
+import com.dimsuz.yamm.login.server_select.ServerSelectController
+import com.dimsuz.yamm.util.AppConfig
+import com.dimsuz.yamm.util.appScope
+import com.dimsuz.yamm.util.instance
 
-class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    val isLoggedIn = false // TODO
-    if (!isLoggedIn) {
-      val intent = Intent(this, LoginActivity::class.java)
-      startActivity(intent)
-      finish()
-      return
+class MainActivity : BaseControllerActivity() {
+
+  override fun createController(): Controller? {
+    val scope = appScope
+    val isLoggedIn = scope.instance<SessionManager>().currentUserId != null
+    return if (!isLoggedIn) {
+      val isServerUrlSet = scope.instance<AppConfig>().getServerUrl() != null
+      if (isServerUrlSet)
+        LoginMethodSelectController()
+      else
+        ServerSelectController()
+    } else {
+      // TODO Messaging controller
+      LoginMethodSelectController()
     }
-    setContentView(R.layout.activity_content_main)
   }
 }
