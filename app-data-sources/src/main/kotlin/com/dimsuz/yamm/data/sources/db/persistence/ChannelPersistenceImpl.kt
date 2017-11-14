af8327c74d9315c3df0eeff61752b0feb88e9c2a
Dmitry Suzdalev <dimsuz@gmail.com>
2017-11-15 00:05:16 +0200

    Implement insert/select functions in ChannelPersistenceImpl

--- app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/ChannelPersistenceImpl.kt
+++ app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/ChannelPersistenceImpl.kt
@@ -1,20 +1,40 @@
 package com.dimsuz.yamm.data.sources.db.persistence
 
 import com.dimsuz.yamm.data.sources.db.models.ChannelDbModel
+import com.dimsuz.yamm.data.sources.db.models.ChannelDbSqlDelightModel
+import com.dimsuz.yamm.data.sources.db.util.executeDelightStatement
+import com.dimsuz.yamm.data.sources.db.util.executeInsert
+import com.dimsuz.yamm.data.sources.db.util.inTransaction
 import com.squareup.sqlbrite2.BriteDatabase
 import io.reactivex.Observable
 
 internal class ChannelPersistenceImpl(private val briteDatabase: BriteDatabase): ChannelPersistence {
 
   override fun replaceUserChannels(channels: List<ChannelDbModel>) {
-    TODO("not implemented")
+    briteDatabase.inTransaction {
+      val insertChannel = ChannelDbSqlDelightModel.Insert_channel(writableDatabase)
+      for (ch in channels) {
+        with(ch) {
+          insertChannel.bind(id, userId, teamId, type, displayName,
+            name, header, purpose, teamMateId)
+          briteDatabase.executeInsert(insertChannel)
+        }
+      }
+    }
   }
 
   override fun getUserChannels(userId: String, teamId: String): List<ChannelDbModel> {
-    TODO("not implemented")
+    return briteDatabase.readableDatabase
+      .executeDelightStatement(
+        ChannelDbModel.FACTORY.user_channels(userId, teamId),
+        ChannelDbModel.FACTORY.user_channelsMapper())
   }
 
   override fun getUserChannelsLive(userId: String, teamId: String): Observable<List<ChannelDbModel>> {
-    TODO("not implemented")
+    val query = ChannelDbModel.FACTORY.user_channels(userId, teamId)
+    val mapper = ChannelDbModel.FACTORY.user_channelsMapper()
+    return briteDatabase
+      .createQuery(ChannelDbSqlDelightModel.TABLE_NAME, query.statement, *query.args)
+      .mapToList(mapper::map)
   }
 }
