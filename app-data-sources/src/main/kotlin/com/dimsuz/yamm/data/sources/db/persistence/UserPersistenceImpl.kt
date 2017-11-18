e67c1dc10798c09e60e06698bea068c58bf8d326
Dmitry Suzdalev <dimsuz@gmail.com>
2017-11-18 20:32:23 +0200

    Execute user insertion in transaction

--- app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/UserPersistenceImpl.kt
+++ app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/UserPersistenceImpl.kt
@@ -3,6 +3,7 @@ package com.dimsuz.yamm.data.sources.db.persistence
 import com.dimsuz.yamm.data.sources.db.models.UserDbModel
 import com.dimsuz.yamm.data.sources.db.models.UserDbSqlDelightModel
 import com.dimsuz.yamm.data.sources.db.util.executeDelightStatement
+import com.dimsuz.yamm.data.sources.db.util.inTransaction
 import com.squareup.sqlbrite2.BriteDatabase
 import javax.inject.Inject
 
@@ -18,10 +19,12 @@ internal class UserPersistenceImpl @Inject constructor(
 
   override fun replaceUsers(users: List<UserDbModel>) {
     val insertOrReplace = UserDbSqlDelightModel.Insert_or_replace(briteDatabase.writableDatabase)
-    for (u in users) {
-      with (u) {
-        insertOrReplace.bind(id, username, firstName, lastName, nickname, email)
-        briteDatabase.executeInsert(insertOrReplace.table, insertOrReplace.program)
+    briteDatabase.inTransaction {
+      for (u in users) {
+        with (u) {
+          insertOrReplace.bind(id, username, firstName, lastName, nickname, email)
+          briteDatabase.executeInsert(insertOrReplace.table, insertOrReplace.program)
+        }
       }
     }
   }
