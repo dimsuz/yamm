b317e39d24fa4ad27681316edf7a29c08d86e2bc
Dmitry Suzdalev <dimsuz@gmail.com>
2017-11-18 16:43:52 +0200

    Implement get/insert for UserPersistenceImpl

--- app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/UserPersistenceImpl.kt
+++ app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/UserPersistenceImpl.kt
@@ -1,14 +1,26 @@
 package com.dimsuz.yamm.data.sources.db.persistence
 
 import com.dimsuz.yamm.data.sources.db.models.UserDbModel
+import com.dimsuz.yamm.data.sources.db.models.UserDbSqlDelightModel
+import com.dimsuz.yamm.data.sources.db.util.executeDelightStatement
+import com.squareup.sqlbrite2.BriteDatabase
 
-internal class UserPersistenceImpl : UserPersistence {
+internal class UserPersistenceImpl(private val briteDatabase: BriteDatabase) : UserPersistence {
 
   override fun getUsersById(ids: List<String>): List<UserDbModel> {
-    TODO("not implemented")
+    return briteDatabase.readableDatabase
+      .executeDelightStatement(
+        UserDbModel.FACTORY.select_by_id(ids.toTypedArray()),
+        UserDbModel.FACTORY.select_by_idMapper())
   }
 
   override fun replaceUsers(users: List<UserDbModel>) {
-    TODO("not implemented")
+    val insertOrReplace = UserDbSqlDelightModel.Insert_or_replace(briteDatabase.writableDatabase)
+    for (u in users) {
+      with (u) {
+        insertOrReplace.bind(id, username, firstName, lastName, nickname, email)
+        briteDatabase.executeInsert(insertOrReplace.table, insertOrReplace.program)
+      }
+    }
   }
 }
