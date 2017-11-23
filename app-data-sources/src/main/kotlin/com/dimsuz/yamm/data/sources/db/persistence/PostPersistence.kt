99a08ee02a0fda250addb3813b08a3dbc0a78860
Dmitry Suzdalev <dimsuz@gmail.com>
2017-11-23 23:58:52 +0200

    Implement basic sql commands for post persistence

--- app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/PostPersistence.kt
+++ app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/PostPersistence.kt
@@ -1,8 +1,9 @@
 package com.dimsuz.yamm.data.sources.db.persistence
 
 import com.dimsuz.yamm.data.sources.db.models.PostDbModel
+import io.reactivex.Observable
 
 interface PostPersistence {
   fun replacePosts(posts: List<PostDbModel>)
-  fun getPostsLive(channelId: String, offset: Int, count: Int): List<PostDbModel>
+  fun getPostsLive(channelId: String, offset: Int, count: Int): Observable<List<PostDbModel>>
 }
--- app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/PostPersistenceImpl.kt
+++ app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/persistence/PostPersistenceImpl.kt
@@ -1,13 +1,30 @@
 package com.dimsuz.yamm.data.sources.db.persistence
 
+import com.dimsuz.yamm.core.util.forEachApply
 import com.dimsuz.yamm.data.sources.db.models.PostDbModel
+import com.dimsuz.yamm.data.sources.db.models.PostDbSqlDelightModel
+import com.dimsuz.yamm.data.sources.db.util.inTransaction
+import com.squareup.sqlbrite2.BriteDatabase
+import io.reactivex.Observable
+import javax.inject.Inject
 
-class PostPersistenceImpl : PostPersistence {
+class PostPersistenceImpl @Inject constructor(
+  private val briteDatabase: BriteDatabase) : PostPersistence {
+
   override fun replacePosts(posts: List<PostDbModel>) {
-    TODO("not implemented")
+    val insertOrReplace = PostDbSqlDelightModel.Insert_or_replace(briteDatabase.writableDatabase)
+    briteDatabase.inTransaction {
+      posts.forEachApply {
+        insertOrReplace.bind(id, userId, channelId, message, type, createAt, updateAt, deleteAt)
+        briteDatabase.executeInsert(insertOrReplace.table, insertOrReplace.program)
+      }
+    }
   }
 
-  override fun getPostsLive(channelId: String, offset: Int, count: Int): List<PostDbModel> {
-    TODO("not implemented")
+  override fun getPostsLive(channelId: String, offset: Int, count: Int): Observable<List<PostDbModel>> {
+    val query = PostDbModel.FACTORY.select_with_offset(channelId, count.toLong(), offset.toLong())
+    val mapper = PostDbModel.FACTORY.select_with_offsetMapper()
+    return briteDatabase.createQuery(query.tables, query.statement, *query.args)
+      .mapToList(mapper::map)
   }
 }
