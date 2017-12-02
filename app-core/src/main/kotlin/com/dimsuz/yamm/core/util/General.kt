9542973f65bc1a398f2d185013f0755c5097c8fb
Dmitry Suzdalev <dimsuz@gmail.com>
2017-12-02 21:30:18 +0200

    Add String.ellipsizeEnd()

--- app-core/src/main/kotlin/com/dimsuz/yamm/core/util/General.kt
+++ app-core/src/main/kotlin/com/dimsuz/yamm/core/util/General.kt
@@ -1,9 +1,15 @@
 package com.dimsuz.yamm.core.util
 
+import kotlin.text.Typography.ellipsis
+
 inline fun <T> Iterable<T>.forEachApply(action: T.() -> Unit) {
   for (e in this) {
     with(e) {
       action()
     }
   }
+}
+
+fun String.ellipsizeEnd(maxLength: Int): String {
+  return if (length > maxLength) this.take(maxLength-1) + ellipsis else this
 }
