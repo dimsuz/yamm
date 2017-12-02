e257b7ef5353d6b9f3755dcd0a76d7f3a10dc116
Dmitry Suzdalev <dimsuz@gmail.com>
2017-12-02 20:01:13 +0200

    Create post DB table during initialization

--- app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/DatabaseHelper.kt
+++ app-data-sources/src/main/kotlin/com/dimsuz/yamm/data/sources/db/DatabaseHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.dimsuz.yamm.data.sources.db.models.ChannelDbSqlDelightModel
+import com.dimsuz.yamm.data.sources.db.models.PostDbSqlDelightModel
 import com.dimsuz.yamm.data.sources.db.models.UserDbSqlDelightModel
 
 private const val DATABASE_NAME = "yamm.db"
@@ -14,6 +15,7 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
   override fun onCreate(db: SQLiteDatabase) {
     db.execSQL(ChannelDbSqlDelightModel.CREATE_TABLE)
     db.execSQL(UserDbSqlDelightModel.CREATE_TABLE)
+    db.execSQL(PostDbSqlDelightModel.CREATE_TABLE)
   }
 
   override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
