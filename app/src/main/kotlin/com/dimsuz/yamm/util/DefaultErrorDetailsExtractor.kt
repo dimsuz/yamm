251b046ecdac82e785743a81284c446577a9e174
Dmitry Suzdalev <dimsuz@gmail.com>
2017-12-17 00:08:17 +0200

    Add logging of errors during details extraction

--- app/src/main/kotlin/com/dimsuz/yamm/util/DefaultErrorDetailsExtractor.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/util/DefaultErrorDetailsExtractor.kt
@@ -1,7 +1,10 @@
 package com.dimsuz.yamm.util
 
+import timber.log.Timber
+
 class DefaultErrorDetailsExtractor : ErrorDetailsExtractor {
   override fun extractErrorText(error: Throwable): String {
+    Timber.e(error)
     return error.message ?: "unknown error"
   }
 }
