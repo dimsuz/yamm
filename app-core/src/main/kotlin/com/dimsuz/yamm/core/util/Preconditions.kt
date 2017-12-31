963ff2e680905e4853995563ed951433d7608c49
Dmitry Suzdalev <dimsuz@gmail.com>
2017-12-31 18:48:23 +0200

    Add Preconditions.checkOrLog

--- app-core/src/main/kotlin/com/dimsuz/yamm/core/util/Preconditions.kt
+++ app-core/src/main/kotlin/com/dimsuz/yamm/core/util/Preconditions.kt
@@ -1,6 +1,8 @@
 package com.dimsuz.yamm.core.util
 
 import android.os.Looper
+import com.dimsuz.yamm.core.BuildConfig
+import timber.log.Timber
 
 fun checkMainThread() {
   check(Looper.getMainLooper() == Looper.myLooper(),
@@ -10,4 +12,15 @@ fun checkMainThread() {
 fun checkWorkerThread() {
   check(Looper.getMainLooper() != Looper.myLooper(),
     { "Expected to be called on worker thread but was on the main thread" })
+}
+
+inline fun checkOrLog(value: Boolean, lazyMessage: () -> Any) {
+  if (BuildConfig.DEBUG) {
+    check(value, lazyMessage)
+  } else {
+    if (!value) {
+      val message = lazyMessage()
+      Timber.e(message.toString())
+    }
+  }
 }
