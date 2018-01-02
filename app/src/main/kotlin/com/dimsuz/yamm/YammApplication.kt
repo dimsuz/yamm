b140e5e6aff7ab532261a8cf500cf70eace87223
Dmitry Suzdalev <dimsuz@gmail.com>
2018-01-02 17:56:56 +0200

    Add Picasso dependency to the project
    Correctly initialize it during startup - it needs a proper instance of
    OkHttpClient which would be able to fetch images while using properly
    set up auth credentials

--- app/src/main/kotlin/com/dimsuz/yamm/YammApplication.kt
+++ app/src/main/kotlin/com/dimsuz/yamm/YammApplication.kt
@@ -5,6 +5,9 @@ import com.dimsuz.yamm.repositories.di.RepositoriesCommonModule
 import com.dimsuz.yamm.repositories.di.RepositoriesModule
 import com.dimsuz.yamm.util.AppConfig
 import com.dimsuz.yamm.util.instance
+import com.squareup.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
+import okhttp3.OkHttpClient
 import ru.terrakok.cicerone.Cicerone
 import timber.log.Timber
 import toothpick.Scope
@@ -15,37 +18,56 @@ class YammApplication : Application() {
     super.onCreate()
 
     configureLogging()
-    configureAppScope()
+    val appScope = configureAppScope()
+    configurePicasso(appScope)
   }
 
-  private fun configureAppScope() {
+  private fun configureAppScope(): Scope {
     val appScope = Toothpick.openScope(this)
     appScope.installModules(
       ApplicationModule(this, Cicerone.create()),
       RepositoriesCommonModule())
     configureUrlDependentDataSources(appScope)
+    return configureUrlDependentDataSources(appScope)
   }
+
+  private fun configurePicasso(appScope: Scope) {
+    // Picasso can be used only once we have a fully configured OkHttpClient
+    if (appScope.name == FULL_APP_SCOPE) {
+      val picasso = Picasso.Builder(this)
+        .downloader(OkHttp3Downloader(appScope.instance<OkHttpClient>()))
+        .build()
+      Picasso.setSingletonInstance(picasso)
+    } else {
+      Timber.d("skipping Picasso configuration, due to not configured full app scope")
+    }
+  }
+
 
   fun onServerUrlChanged() {
     // server url change implies that DataSourcesModule must be reconfigured.
     // using technique described in this post:
     // https://stackoverflow.com/questions/46760226/is-there-a-way-to-re-add-a-module-to-a-scope-in-a-toothpick-di-library
     Toothpick.closeScope(FULL_APP_SCOPE)
-    configureUrlDependentDataSources(Toothpick.openScope(this))
+    val appScope = configureUrlDependentDataSources(Toothpick.openScope(this))
+    // also reconfigure other services depending on new scope
+    configurePicasso(appScope)
   }
 
-  private fun configureUrlDependentDataSources(scope: Scope) {
+  private fun configureUrlDependentDataSources(scope: Scope): Scope {
     val serverUrl = scope.instance<AppConfig>().getServerUrl()
-    if (serverUrl != null) {
+    return if (serverUrl != null) {
       Timber.d("Configuring data sources module: $serverUrl")
-      val newScope = Toothpick.openScopes(this, FULL_APP_SCOPE)
-      newScope.installModules(
-        RepositoriesModule(serverUrl),
-        AuthorizedApplicationModule()
-      )
+      Toothpick.openScopes(this, FULL_APP_SCOPE).apply {
+        installModules(
+          RepositoriesModule(serverUrl),
+          AuthorizedApplicationModule()
+        )
+      }
     } else {
       Timber.d("Network config is not available, skipping configuration for now")
       Toothpick.closeScope(FULL_APP_SCOPE)
+      scope
     }
   }
 
