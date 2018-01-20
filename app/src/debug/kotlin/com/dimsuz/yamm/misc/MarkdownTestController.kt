2790e600667af238f48ec9ab6334a060684945ba
Dmitry Suzdalev <dimsuz@gmail.com>
2018-01-21 00:08:00 +0200

    Add a test controller for testing markdown rendering

---
+++ app/src/debug/kotlin/com/dimsuz/yamm/misc/MarkdownTestController.kt
@@ -0,0 +1,14 @@
+package com.dimsuz.yamm.misc
+
+import android.view.View
+import com.dimsuz.yamm.R
+import com.dimsuz.yamm.presentation.baseui.BaseController
+
+class MarkdownTestController : BaseController() {
+  override fun getViewLayout(): Int {
+    return R.layout.markdown_test
+  }
+
+  override fun initializeView(rootView: View) {
+  }
+}
