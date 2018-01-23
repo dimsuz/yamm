e2ae214099d73a5abbbf0e3f95b419523f3430b8
Dmitry Suzdalev <dimsuz@gmail.com>
2018-01-23 23:43:17 +0200

    Add some sample strings to render

--- app/src/debug/kotlin/com/dimsuz/yamm/misc/MarkdownTestController.kt
+++ app/src/debug/kotlin/com/dimsuz/yamm/misc/MarkdownTestController.kt
@@ -1,14 +1,24 @@
 package com.dimsuz.yamm.misc
 
 import android.view.View
+import com.dimsuz.markdown.YammMarkdownRenderer
 import com.dimsuz.yamm.R
 import com.dimsuz.yamm.presentation.baseui.BaseController
+import kotlinx.android.synthetic.debug.markdown_test.*
 
 class MarkdownTestController : BaseController() {
+  private val renderer = YammMarkdownRenderer()
+
   override fun getViewLayout(): Int {
     return R.layout.markdown_test
   }
 
   override fun initializeView(rootView: View) {
+    primaryButton.setOnClickListener { render(editText.text.toString()) }
+    renderSampleButton.setOnClickListener { render(TEST_SIMPLE) }
+  }
+
+  private fun render(s: String) {
+    markdownOutputView.text = renderer.render(s)
   }
 }
